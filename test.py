14e0f6f0250d02d2a169e78ce04e420f0d4e92a5
fedorchukdmytro <fedorchuk.dmytro@ukr.net>
2024-04-16 16:10:08 +0300

    4 commit

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 
 chrome_options = Options()
-# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument("--window-size=1920,1080")
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36")
